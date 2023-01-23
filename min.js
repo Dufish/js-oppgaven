f2647fd386f7c8e9744740de54194d48bf323824
Dufish <111867500+Dufish@users.noreply.github.com>
2023-01-23 19:59:35 +0100

    Add files via upload

---
+++ min.js
@@ -0,0 +1,35 @@
+const car ={
+brand:"samsung",
+speed:0,
+revs:0,
+kilometers: 1000,
+cc:456,
+on:true,
+gearbox:["Neutral","first","second","Third"],}
+const fruit = { brand: {
+    name:"Banana",
+    hq:"England",
+},
+
+}
+const bike = { 
+    hands: {
+        name: "Neutral",
+        ratio:0
+    },
+}
+{name:"first",
+ratio; "1"
+}
+{
+name:"First"
+ratio: 4.7
+}
+{
+    name:"These",
+    ratio; 6
+}
+{
+name: "First"
+ratio; 4.7
+}
